77721f840007885fdff32aa59e9725b2451093a8
Mark Lawson <mark@walkonmars.co.uk>
2022-08-09 19:53:46 +0100

    setup geolocation in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
+import React, {useState} from "react";
 import "./App.scss";
 
-import React from "react";
-
 const App = () => {
-  return <div>App</div>;
+  const [latitude, setLatitude] = useState(null);
+  const [Longitude, setLongitude] = useState(null);
+  const [status, setStatus] = useState(null);
+
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setStatus("Geolocation is not supported by your browser");
+    } else {
+      setStatus("Locating...");
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setStatus(null);
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
+        },
+        () => {
+          setStatus("Unable to retrieve your location");
+        }
+      );
+    }
+  };
+
+  return (
+    <div className="App__container">
+      <button onClick={getLocation}>Get Location</button>
+      <h1>Coordinates</h1>
+      <p>{status}</p>
+      {latitude && <p>Latitude: {latitude}</p>}
+      {Longitude && <p>Longitude: {Longitude}</p>}
+    </div>
+  );
 };
 
 export default App;
