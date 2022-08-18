cce0bf5212c991147506468df294196c255ba6fe
Mark Lawson <mark@walkonmars.co.uk>
2022-08-18 13:53:38 +0100

    added background to app

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ const App = () => {
   const GEO_KEY = process.env.REACT_APP_GEO_KEY;
   const units = "metric";
 
+  // geolocation
   useEffect(() => {
     if (!navigator.geolocation) {
       setLoading("Geolocation is not supported by your browser");
@@ -31,6 +32,7 @@ const App = () => {
     }
   }, []);
 
+  // reverse geocoding
   useEffect(() => {
     if (latitude && longitude)
       fetch(`http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&appid=${GEO_KEY}`)
@@ -43,6 +45,7 @@ const App = () => {
         });
   }, [GEO_KEY, latitude, longitude]);
 
+  // weather
   useEffect(() => {
     if (locationResult) {
       const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&units=${units}&appid=${API_KEY}`;
@@ -60,35 +63,51 @@ const App = () => {
     }
   }, [locationResult, latitude, longitude, API_KEY]);
 
-  console.log(weather);
+  const currentHour = new Date().getHours();
+  let greetingImg = "";
+  let greetingTime = "Morning";
+
+  if (currentHour >= 12) {
+    greetingImg = "";
+    greetingTime = "Afternoon";
+  }
+
+  if (currentHour >= 18) {
+    greetingImg = "";
+    greetingTime = "Evening";
+  }
 
   return (
-    <div className="app__container">
-      <h3>{loading}</h3>
-      <h2>{locationResult && locationResult[0].name}</h2>
-      <p>Currently - {weather && weather.current.weather[0].description && Math.round(weather.current.temp)}&#176;C</p>
-      <p>{weather && weather.current.weather[0].description}</p>
-      <div>
-        {weather &&
-          weather.daily.map((day) => {
-            const dayTime = new Date(day.dt * 1000).toDateString();
-            const temp = Math.round(day.temp.day);
-            const sunrise = new Date(day.sunrise * 1000).toTimeString().slice(0, 5);
-            const sunset = new Date(day.sunset * 1000).toTimeString().slice(0, 5);
+    <div className="app">
+      <div className="app__container">
+        <h1>Good {greetingTime}</h1>
+        <h3>{loading}</h3>
+        <h3>Here is the current weather for...</h3>
+        <h2>{locationResult && locationResult[0].name}</h2>
+        <p>Currently - {weather && Math.round(weather.current.temp)}&#176;C</p>
+        <p>{weather && weather.current.weather[0].description}</p>
+        <div>
+          {weather &&
+            weather.daily.map((day) => {
+              const dayTime = new Date(day.dt * 1000).toDateString();
+              const temp = Math.round(day.temp.day);
+              const sunrise = new Date(day.sunrise * 1000).toTimeString().slice(0, 5);
+              const sunset = new Date(day.sunset * 1000).toTimeString().slice(0, 5);
 
-            return (
-              <div className="daily-weather" key={day.dt}>
-                <p>{dayTime}</p>
+              return (
+                <div className="daily-weather" key={day.dt}>
+                  <p>{dayTime}</p>
 
-                <img src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`} alt={day.weather[0].description} />
+                  <img src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png`} alt={day.weather[0].description} />
 
-                <p>{day.weather[0].description.charAt(0).toUpperCase() + day.weather[0].description.slice(1)}</p>
-                <p>High {temp}&#176;C </p>
-                <p>Sunrise {sunrise}</p>
-                <p>Sunset {sunset}</p>
-              </div>
-            );
-          })}
+                  <p>{day.weather[0].description.charAt(0).toUpperCase() + day.weather[0].description.slice(1)}</p>
+                  <p>High {temp}&#176;C </p>
+                  <p>Sunrise {sunrise}</p>
+                  <p>Sunset {sunset}</p>
+                </div>
+              );
+            })}
+        </div>
       </div>
     </div>
   );
